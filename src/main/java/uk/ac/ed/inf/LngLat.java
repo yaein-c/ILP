4760f6f6eb6bed2cf991a50f7db873d2238f9954
Yan <yaneechin@gmail.com>
2022-09-24 23:52:39 +0100

    fix + javadoc of LngLat
    fixed the distanceTo and closeTo methods and added the javadoc comments to LngLat

--- src/main/java/uk/ac/ed/inf/LngLat.java
+++ src/main/java/uk/ac/ed/inf/LngLat.java
@@ -2,18 +2,35 @@ package uk.ac.ed.inf;
 
 public record LngLat(double lng, double lat){
 
+    /**
+     * @return true if current location is within central area and false otherwise
+     */
     public boolean inCentralArea(){
         return false;
     }
 
-    public double distanceTo(LngLat a, LngLat b){
-        return Math.sqrt((a.lng - b.lng)*(a.lng - b.lng) + (a.lat - b.lat)*(a.lat - b.lat));
+    /**
+     * @param otherLocation coordinates of another location
+     * @return distance from current location to other location
+     */
+    public double distanceTo(LngLat otherLocation){
+        return Math.sqrt((lng-otherLocation.lng)*(lng-otherLocation.lng)+(lat-otherLocation.lat)*(lat-otherLocation.lat));
     }
 
-    public boolean closeTo(LngLat a, LngLat b){
+    /**
+     * Checks whether current location is close to other location
+     * @param otherLocation coordinates of next location
+     * @return true if current location and other location are close and false otherwise
+     */
+    public boolean closeTo(LngLat otherLocation){
         return false;
     }
 
+    /**
+     * Returns coordinates of new location after a move in a compass direction
+     * @param direction given in degrees from 0 to 360
+     * @return coordinates of the new location of the drone
+     */
     public LngLat nextPosition(int direction){
         return null;
     }
