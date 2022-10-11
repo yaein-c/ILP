3d511bf1bd24bae70c80e11b78f70c631a295dfa
Yan <yaneechin@gmail.com>
2022-10-11 22:44:16 +0100

    Completed getDeliveryCost
    Class Order completed and ready for testing

--- src/main/java/uk/ac/ed/inf/Order.java
+++ src/main/java/uk/ac/ed/inf/Order.java
@@ -2,6 +2,9 @@ package uk.ac.ed.inf;
 
 import java.util.ArrayList;
 
+/**
+ * Orders do something.
+ */
 public class Order {
     public enum OrderOutcome {
         Delivered ,
@@ -18,39 +21,48 @@ public class Order {
     private OrderOutcome status;
 
     /**
-     * Getter method for status of OrderOutcome
+     * Getter method for status of OrderOutcome.
      * @return status
      */
     public OrderOutcome getStatus() { return status; }
 
     /**
-     * Setter method for status of OrderOutcome
+     * Setter method for status of OrderOutcome.
      * @param status
      */
     public void setStatus(OrderOutcome status) { this.status = status; }
 
+    /**
+     * Finds and returns the restaurant associated with the first order.
+     * @param restaurants
+     * @param orders
+     * @return restaurant associated with first order
+     */
+    public Restaurant getCurrentRestaurant(Restaurant[] restaurants, String[] orders) {
+        Restaurant currentRestaurant = null;
+
+        mainLoop:
+        for (Restaurant r : restaurants) {
+            for (Menu item : r.getMenu()) {
+                if (item.getName().equalsIgnoreCase(orders[0])) {
+                    currentRestaurant = r;
+                    break mainLoop;
+                }
+            }
+        }
+        return currentRestaurant;
+    }
 
     /**
-     * Throws exception if the ordered pizza combination cannot be delivered by the same restaurant
+     * Throws exception if the ordered pizza combination cannot be delivered by the same restaurant.
      * @param restaurants
      * @param orders
      * @throws InvalidPizzaCombinationException
      */
      public void checkPizzaCombination(Restaurant[] restaurants, String[] orders) throws InvalidPizzaCombinationException {
-         Restaurant currentRestaurant = null;
+         var currentRestaurant = getCurrentRestaurant(restaurants, orders);
 
-         //find restaurant associated with first order
-         mainLoop:
-         for (Restaurant r : restaurants) {
-             for (Menu item : r.getMenu()) {
-                 if (item.getName().equalsIgnoreCase(orders[0])) {
-                     currentRestaurant = r;
-                     break mainLoop;
-                 }
-             }
-         }
-
-         //check if all orders come from same restaurant as first
+         //check if all orders come from same restaurant
          int counter = 0;
          for (String order : orders) {
              for (Menu item : currentRestaurant.getMenu()) {
@@ -69,7 +81,7 @@ public class Order {
     }
 
     /**
-     * Checks if all pizzas are defined regardless of which restaurant the pizza is served by
+     * Checks if all pizzas are defined regardless of which restaurant the pizza is served by.
      * @param restaurants
      * @param orders
      * @return true if one or more pizzas undefined
@@ -92,72 +104,39 @@ public class Order {
          return validPizzaCount != orders.length;
      }
 
-
     /**
-     *
+     * Checks if order is valid and calculates total cost of order plus delivery charge.
      * @param restaurants array of the participating restaurants in the order
      * @param orders array of names of each individual pizza ordered
-     * @return total cost including the pizzas and the delivery charge.
+     * @return total cost plus delivery in pence
      */
         public int getDeliveryCost(Restaurant[] restaurants, String[] orders){
 
-            //get array of all menu items
-            Menu[] allMenus = getAllMenus(restaurants);
-            String[] names = new String[allMenus.length];
-            for (int i = 0; i < allMenus.length; i++) {
-                names[i] = allMenus[i].getName();
-            }
-
-            //check if pizza exists and store in bool
-            var list = new ArrayList<>();
-            for (String i : orders) {
-                for (String item : names) {
-                    if (item.equalsIgnoreCase(i)) {
-                        list.add(1);
-                    }
-                }
-
-            }
-            boolean pizzaExists = list.size() == orders.length;
-
-            //invalid pizza count
             if ( (orders.length < 1) || (orders.length > 4) ) {
                 setStatus(OrderOutcome.InvalidPizzaCount);
+                return 1;
             }
-            //pizza doesn't exist
-            else if (pizzaExists) {
+            try {
+                checkPizzaCombination(restaurants, orders);
+            } catch( InvalidPizzaCombinationException e) {
+                e.printStackTrace();
+                return 1;
+            }
+            if (checkPizzaDefined(restaurants, orders)) {
                 setStatus(OrderOutcome.InvalidPizzaNotDefined);
-
-            //all good calculate cost
-            } else {
-                int cost = 100; //cost in pence
-                Restaurant currentRestaurant = null;
-                Menu[] currentMenu;
-
-                //find restaurant that does these items
-                for (Restaurant r : restaurants) {
-                    for (Menu m : r.getMenu() ) {
-                        if (m.getName().equalsIgnoreCase(orders[0])) {
-                            currentRestaurant = r;
-                        }
-                    }
-                }
-
-                //find cost of each order and add to delivery cost
-                currentMenu = currentRestaurant.getMenu();
-                for (String i : orders ) {
-                    for (Menu j : currentMenu ) {
-                        if ( j.getName().equalsIgnoreCase(i) ) {
-                            cost += j.getPriceInPence();
-                        }
-                    }
-                }
-                return cost;
+                return 1;
             }
-            //something has gone wrong
-            setStatus(OrderOutcome.Invalid);
-            return 0;
-
+            int totalCost = 100; //cost in pence
+            var currentRestaurant = getCurrentRestaurant(restaurants, orders);
+            for (String order : orders) {
+                for (Menu item : currentRestaurant.getMenu()) {
+                    if (item.getName().equalsIgnoreCase(order)) {
+                        totalCost += item.getPriceInPence();
+                        break;
+                    }
+                }
+            }
+            return totalCost;
         }
 
     }
